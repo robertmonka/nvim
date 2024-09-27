a6bf30fc9e15e96b524be7d79b42572040f25e63
Robert Mońka <robert.monka@versanis.pl>
2024-09-27 09:13:38 +0200

    update/ easy-coding-standard

--- lua/robert/plugins/linting.lua
+++ lua/robert/plugins/linting.lua
@@ -12,7 +12,7 @@ return {
 			-- typescriptreact = { "eslint_d" },
 			-- svelte = { "eslint_d" },
 			python = { "pylint" },
-			-- php = { "phpmd" },
+			php = { "easy-coding-standard" },
 			yaml = { "yamllint" },
 			dockerfile = { "hadolint" },
 			-- json = { "jsonlint", "cfn-lint" },
