2a835fe1ceb4a4f6ca51cc16319526179eb71eb6
Robert Mońka <robert.monka@versanis.pl>
2024-09-27 09:49:47 +0200

    update/ treesitter

--- lua/robert/plugins/nvim-treesitter.lua
+++ lua/robert/plugins/nvim-treesitter.lua
@@ -8,34 +8,18 @@ return {
 			"windwp/nvim-ts-autotag",
 		},
 		config = function()
-			-- import nvim-treesitter plugin
 			local treesitter = require("nvim-treesitter.configs")
 
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				highlight = {
-					enable = true,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = true,
-				},
-				-- ensure these language parsers are installed
+			treesitter.setup({
+				modules = {},
 				ensure_installed = {
 					"json",
 					"javascript",
-					-- "typescript",
-					-- "tsx",
 					"yaml",
 					"html",
 					"css",
-					-- "prisma",
 					"markdown",
 					"markdown_inline",
-					-- "svelte",
-					-- "graphql",
 					"bash",
 					"lua",
 					"vim",
@@ -45,6 +29,15 @@ return {
 					"php",
 					"sql",
 				},
+				sync_install = false,
+				ignore_install = {},
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+				autotag = { enable = true },
 				incremental_selection = {
 					enable = true,
 					keymaps = {
