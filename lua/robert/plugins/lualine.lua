0aa6b288f6c3d1e7a6c9f55df68dd207d069737d
Robert Mońka <robert.monka@versanis.pl>
2025-06-07 19:14:03 +0200

    update

--- lua/robert/plugins/lualine.lua
+++ lua/robert/plugins/lualine.lua
@@ -3,31 +3,19 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		local lazy_status = require("lazy.status")
 
 		lualine.setup({
 			options = {
-				disabled_filetypes = { "NvimTree" },
+				disabled_filetypes = {
+					statusline = {
+						"NvimTree",
+						"Avante",
+						"AvanteSelectedFiles",
+						"AvanteInput",
+					},
+				},
 			},
-			-- sections = {
-			-- 	lualine_c = {
-			-- 		{
-			-- 			"filename",
-			-- 			file_status = true,
-			-- 			newfile_status = false,
-			-- 			path = 0, --3 to full path
-			--
-			-- 			shorting_target = 40,
-			--
-			-- 			symbols = {
-			-- 				modified = "[+]",
-			-- 				readonly = "[-]",
-			-- 				unnamed = "[No Name]",
-			-- 				newfile = "[New]",
-			-- 			},
-			-- 		},
-			-- 	},
-			-- },
 		})
 	end,
 }
